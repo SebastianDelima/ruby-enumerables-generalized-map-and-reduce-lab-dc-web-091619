47fd95f3720872725a425c4e6176a4aafafe72bc
Sebastian Delima <sebastiandelimagec@gmail.com>
2019-09-11 17:40:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,20 @@
-# Your Code Here
+require 'pry'
+
+def map(array) 
+  i = 0 
+  array1 = []
+  while i < array.length do
+ array1.push(yield(array[i]))
+ i += 1
+ end
+ return array1
+end
+
+# map([1,2,3,4]){|num| num + 1}  [2,3,4,5]
+# map([1,2,3,4]){|num| num + 2}  [3,4,5,6]
+ 
+def reduce(array, starting_point = 0)
+  array1 = starting_point
+  i = 0 
+  while i < array.length do
+    
