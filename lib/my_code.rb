db4fe6d87383146cec0fa60fcc21a948756206b1
Sebastian Delima <sebastiandelimagec@gmail.com>
2019-09-11 17:42:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,9 +13,28 @@ end
 # map([1,2,3,4]){|num| num + 1}  [2,3,4,5]
 # map([1,2,3,4]){|num| num + 2}  [3,4,5,6]
  
- 
+# def reduce_to_total(source_array, starting_point = 0)
+#   new = 0
+#   counter = 0 
+#   while counter < source_array.length do
+#   new += source_array[counter] 
+#   counter += 1
+#   end
+#   return 
+# end
+
 def reduce(array, starting_point = 0)
   array1 = starting_point
   i = 0 
   while i < array.length do
+    yield
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
