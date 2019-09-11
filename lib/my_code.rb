1feb6fcc7295eee9c769904576c8e2f87b4e8281
Sebastian Delima <sebastiandelimagec@gmail.com>
2019-09-11 18:02:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -27,10 +27,11 @@ def reduce(array, starting_point = 0)
   total = starting_point
   i = 0 
   while i < array.length do
-    
-    
-    
+    total += array[i]
+    i += 1 
   end
+  return total
+end
     
     
     
